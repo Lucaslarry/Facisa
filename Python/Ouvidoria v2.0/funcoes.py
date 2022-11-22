6c5aa88bda907790bb4018fddcd669e11c1a3a8f
Your Name <116319211+Lucaslarry@users.noreply.github.com>
2022-11-22 11:11:11 -0300

    update

--- Python/Ouvidoria v2.0/funcoes.py
+++ Python/Ouvidoria v2.0/funcoes.py
@@ -1,13 +1,14 @@
 from time import sleep #só pra não ficar brotando tudo de uma vez na tela
 class Ouvidoria:
-    def __init__(self):
+    elogios= []
+    criticas= []
+    sugestoes= []
+
+
+    def __init__(self,nome):
         """Iniciador da classe ouvidoria, ele inicia perguntando seu nome e dando boas vindas, logo após cria as listas que você vai usar para guardar as informações
         """
-        self.nome= str(input('Para iniciar a ouvidoria digite seu nome: ')).strip().title()
-        print('\033[1;35m*\033[m' * 5, f'Seja bem vindo a nossa ouvidoria, {self.nome}', '\033[1;35m*\033[m' * 5)
-        self.elogios= []
-        self.criticas= []
-        self.sugestoes= []
+        print('\033[1;35m*\033[m' * 5, f'Seja bem vindo a nossa ouvidoria, {nome}', '\033[1;35m*\033[m' * 5)
 
 
     def voltando(self):
@@ -55,34 +56,9 @@ class Ouvidoria:
             sleep(0.1)
         print('='*60)
         sleep(0.5)
-        try:
-            op=int(input('O que deseja fazer? '))
-            if op>len(opcoes):
-                Ouvidoria.msgerro(self, 'Você não digitou uma opção valida')
-        except:
-            Ouvidoria.msgerro(self,'Você não digitou uma opção valida')
-        else:
-            return op
+        
 
-
-    def fazerdnv(self,msg):
-        """pergunta ao usuario se ele quer fazer a msm ação de novo
-
-        Args:
-            msg (string): a pergunta 
-
-        Returns:
-            int: caso seja igual a 2 ele volta pro menu principal
-        """
-        while True:
-            x= Ouvidoria.menu(self,[msg,'Sair'])
-            if x==1:
-                break
-            elif x==2:
-                return x
-
-
-    def fazercoment(self,nome,tipo,seus='sua',art='A'):
+    def fazercoment(self,nome,tipo,coment,seus='sua',art='A'):
         """Função pra adicionar comentário em uma das listas
 
         Args:
@@ -91,29 +67,8 @@ class Ouvidoria:
             seus (str, optional): só pra personalização. Defaults to 'sua'.
             art (str, optional): tbm pra personalização. Defaults to 'A'.
         """
-        coment = str(input(f'Por favor digite {seus} {tipo}: '))
         nome.append(coment)
         Ouvidoria.msgsucesso(self,f'{art} {seus} {tipo} foi resgistrado')
-
-
-    def comentarios(self):
-        """função principal dos comentários, aqui apresenta um menu onde o usuario escolhe onde quer adicionar o comentário e passa como parametro pra função fazercoment
-        """
-        while True:
-            x = Ouvidoria.menu(self,['Elogios', 'Criticas', 'Sugestões', 'Voltar'])
-            if x == 1:
-                Ouvidoria.fazercoment(self,self.elogios,'elogio','seu','O')
-            elif x == 2:
-                Ouvidoria.fazercoment(self,self.criticas,'critica')
-            elif x == 3:
-                Ouvidoria.fazercoment(self,self.sugestoes,'sugestão')
-            elif x == 4:
-                Ouvidoria.voltando(self)
-                break
-            if x in (1,2,3):
-                y = Ouvidoria.fazerdnv(self, 'Fazer mais algum comentário')
-                if y==2:
-                    break
 
 
     def vercoment(self,nome,tipo,cor='7',seus='Suas'):
@@ -134,31 +89,7 @@ class Ouvidoria:
             id += 1
 
 
-    def ver(self):
-        """menu principal da função ver, aqui que o usuario escolhe qual lista ele quer ver e passa como parametro pra função vercoment
-        """
-        while True:
-            x = Ouvidoria.menu(self,['Ver seus elogios', 'Ver suas Criticas', 'Ver suas Sugestões', 'Ver tudo', 'Voltar'])
-            if x == 1:
-                Ouvidoria.vercoment(self,self.elogios,'elogios','2','Seus')
-            elif x == 2:
-                Ouvidoria.vercoment(self,self.criticas,'criticas','1')
-            elif x == 3:
-                Ouvidoria.vercoment(self,self.sugestoes,'sugestões','3')
-            elif x==4:
-                Ouvidoria.vercoment(self,self.elogios,'elogios','2','Seus')
-                Ouvidoria.vercoment(self,self.criticas,'criticas','1')
-                Ouvidoria.vercoment(self,self.sugestoes,'sugestões','3')
-            elif x == 5:
-                Ouvidoria.voltando(self)
-                break
-            if x in (1, 2, 3, 4):
-                y = Ouvidoria.fazerdnv(self, 'Ver mais algum comentário?')
-                if y == 2:
-                    break
-
-
-    def apagarcoment(self,nome,tipover,cor='7',seus='Suas',art='as'):
+    def apagarcoment(self,nome,tipover,comentario,art='a'):
         """função pra apagar apenas um comentário da lista
 
         Args:
@@ -168,68 +99,9 @@ class Ouvidoria:
             seus (str, optional): personalização só. Defaults to 'Suas'.
             art (str, optional): personalização tbm. Defaults to 'a'.
         """
-        Ouvidoria.vercoment(self,nome,tipover,cor,seus)
         try:
-            apag = int(input(f'Qual ID d{art} {tipover} que você deseja apagar? '))
+            nome.pop(comentario - 1)
         except:
             Ouvidoria.msgerro(self,f'Esse {tipover} não existe')
         else:
-            try:
-                nome.pop(apag - 1)
-            except:
-                Ouvidoria.msgerro(self,f'Esse {tipover} não existe')
-            else:
-                Ouvidoria.msgsucesso(self,f'{tipover} apagad{art}')
-
-
-    def apagartudo(self):
-        """função para apagar completamente o que tem dentro de uma lista que o usuario escolher
-        """
-        k= Ouvidoria.menu(self,['Apagar todos os elogios','Apagar todas as criticas','Apagar todas as sugestões','Apagar tudo','Voltar pro menu principal'])
-        if k==1:
-            self.elogios.clear()
-            Ouvidoria.msgsucesso(self,'Todos os elogios apagados')
-        elif k==2:
-            self.criticas.clear()
-            Ouvidoria.msgsucesso(self, 'Todas as criticas apagados')
-        elif k==3:
-            self.sugestoes.clear()
-            Ouvidoria.msgsucesso(self, 'Todas as sugestões apagados')
-        elif k==4:
-            self.elogios.clear()
-            self.criticas.clear()
-            self.sugestoes.clear()
-            Ouvidoria.msgsucesso(self, 'Todos os comentários apagados')
-        elif k==5:
-            Ouvidoria.voltando(self)
-            print()
-        if k in range(1,2,3):
-           while True:
-                y = Ouvidoria.fazerdnv(self, 'apagar mais alguma coisa')
-                if y==2:
-                    break
-
-
-
-    def apagar(self):
-        """Menu principal da função de apagar, aqui o usuario escolhe se quer apagar um comentário ou todos de uma lista 
-        """
-        while True:
-            x = Ouvidoria.menu(self,['Apagar apenas um comentário', 'Apagar tudo', 'Voltar'])
-            if x==1:
-                menu = Ouvidoria.menu(self,['Apagar um elogio', 'Apagar uma critica', 'Apagar uma sugestão', 'Voltar pro menu principal'])
-                if menu==1:
-                    Ouvidoria.apagarcoment(self,self.elogios,'elogios','2','Seus','os')
-                elif menu==2:
-                    Ouvidoria.apagarcoment(self,self.criticas,'criticas','1')
-                elif menu==3:
-                    Ouvidoria.apagarcoment(self,self.sugestoes,'sugestões','3')
-                if menu in (1, 2, 3):
-                    y = Ouvidoria.fazerdnv(self, 'apagar mais alguma coisa')
-                    if y==2:
-                        break
-            elif x==2:
-                Ouvidoria.apagartudo(self)
-            elif x==3:
-                Ouvidoria.voltando(self)
-                break
+            Ouvidoria.msgsucesso(self,f'{tipover} apagad{art}')
--- Python/Ouvidoria v2.0/ouvidoria_main v2.1.py
+++ Python/Ouvidoria v2.0/ouvidoria_main v2.1.py
@@ -1,15 +1,104 @@
 import funcoes
-
-ouvidoria = funcoes.Ouvidoria()
+nome= str(input('Para iniciar a ouvidoria digite seu nome: ')).strip().title()
+ouvidoria= funcoes.Ouvidoria(nome)
 
 while True:
-    Menu= ouvidoria.menu(['Cadastrar Comentários','Ver Comentários','Apagar Comentários','Sair'])
-    if Menu==1:
-        ouvidoria.comentarios()
-    if Menu==2:
-        ouvidoria.ver()
-    if Menu==3:
-        ouvidoria.apagar()
-    if Menu==4:
-        print('Obrigado pelo Feedback')
+    ouvidoria.menu(['Cadastrar Comentários','Ver Comentários','Apagar Comentários','Sair'])
+    menu= str(input('O que deseja fazer? '))
+    if menu=='1':
+        ouvidoria.menu(['Elogios','Criticas','Sugestões','Voltar'])
+        menucoment= str(input('O que deseja fazer? '))
+        if menucoment=='1':
+            comentario= str(input('Digite seu elogio: '))
+            ouvidoria.fazercoment(ouvidoria.elogios,'elogio',comentario,'seu','O')
+        elif menucoment=='2':
+            comentario= str(input('Digite sua critica: '))
+            ouvidoria.fazercoment(ouvidoria.criticas,'critica', comentario)
+        elif menucoment=='3':
+            comentario= str(input('Digite sua sugestão: '))
+            ouvidoria.fazercoment(ouvidoria.sugestoes,'sugestão', comentario)
+        elif menucoment=='4':
+            ouvidoria.voltando()
+        else:
+            ouvidoria.msgerro('Você não escolheu uma opção valida')
+    elif menu=='2':
+        ouvidoria.menu(['Ver elogios','Ver criticas','Ver Sugestões','Ver tudo','Voltar'])
+        menuver=str(input('O que deseja fazer? '))
+        if menuver=='1':
+            ouvidoria.vercoment(ouvidoria.elogios,'elogios','2','Seus')
+        elif menuver=='2':
+            ouvidoria.vercoment(ouvidoria.criticas,'criticas','1')
+        elif menuver=='3':
+            ouvidoria.vercoment(ouvidoria.sugestoes,'sugestões','3')
+        elif menuver=='4':
+            ouvidoria.vercoment(ouvidoria.elogios,'elogios','2','Seus')
+            ouvidoria.vercoment(ouvidoria.criticas,'criticas','1')
+            ouvidoria.vercoment(ouvidoria.sugestoes,'sugestões','3')
+        elif menuver=='5':
+            ouvidoria.voltando()
+        else:
+            ouvidoria.msgerro('Você não escolheu uma opção valida')
+    elif menu=='3':
+        ouvidoria.menu(['Apagar um comentário','Apagar todos os comentários','Voltar'])
+        menuapagar= str(input('O que deseja Fazer? '))
+        if menuapagar=='1':
+            ouvidoria.menu(['Apagar um elogio', 'Apagar uma critica', 'Apagar uma sugestão', 'Voltar pro menu principal'])
+            menuapagar2= str(input('O que deseja fazer? '))
+            if menuapagar2=='1':
+                ouvidoria.vercoment(ouvidoria.elogios,'elogios','2','Seus')
+                try:
+                    apag = int(input(f'Qual ID do elogio que você deseja apagar? '))
+                except:
+                    ouvidoria.msgerro(f'Esse elogio não existe')
+                else:
+                    ouvidoria.apagarcoment(ouvidoria.elogios,'elogio',apag,'o')         
+            elif menuapagar2=='2':
+                ouvidoria.vercoment(ouvidoria.criticas,'criticas','1')
+                try:
+                    apag = int(input(f'Qual ID da critica que você deseja apagar? '))
+                except:
+                    ouvidoria.msgerro(f'Essa critica não existe')
+                else:
+                    ouvidoria.apagarcoment(ouvidoria.criticas,'critica',apag)
+            elif menuapagar2=='3':
+                ouvidoria.vercoment(ouvidoria.sugestoes,'sugestões','3')
+                try:
+                    apag = int(input(f'Qual ID da sugestão que você deseja apagar? '))
+                except:
+                    ouvidoria.msgerro(f'Essa sugestão não existe')
+                else:
+                    ouvidoria.apagarcoment(ouvidoria.sugestoes,'sugestão',apag)
+            elif menuapagar2=='4':
+                ouvidoria.voltando()
+            else:
+                ouvidoria.msgerro('Você não escolheu uma opção valida')
+        elif menuapagar=='2':
+            ouvidoria.menu(['Apagar todos os elogios','Apagar todas as criticas','Apagar todas as sugestões','Apagar tudo','Voltar pro menu principal'])
+            menuapagar3=str(input('O que deseja fazer? '))
+            if menuapagar3=='1':
+                ouvidoria.elogios.clear()
+                ouvidoria.msgsucesso('Todos os elogios apagados')
+            elif menuapagar3=='2':
+                ouvidoria.criticas.clear()
+                ouvidoria.msgsucesso('Todas as criticas apagadas')
+            elif menuapagar3=='3':
+                ouvidoria.sugestoes.clear()
+                ouvidoria.msgsucesso('Todas as sugestões apagadas')
+            elif menuapagar3=='4':
+                ouvidoria.elogios.clear()
+                ouvidoria.criticas.clear()
+                ouvidoria.sugestoes.clear()
+                ouvidoria.msgsucesso('Todas os comentários apagados')
+            elif menuapagar3=='5':
+                ouvidoria.voltando()
+            else:
+                ouvidoria.msgerro('Você não escolheu uma opção valida')
+        elif menuapagar=='3':
+            ouvidoria.voltando()
+        else:
+            ouvidoria.msgerro('Você não escolheu uma opção valida')
+    elif menu=='4':
+        print('Obrigado pelo feedback')
         break
+    else:
+        ouvidoria.msgerro('Você não escolheu uma opção valida')
