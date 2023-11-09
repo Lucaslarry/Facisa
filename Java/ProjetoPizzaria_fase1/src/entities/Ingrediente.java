5e054991566e5f4ce2edae426e0d05132842d96a
Lucas Larry <lucasslarryy@gmail.com>
2023-11-09 16:51:53 -0300

    fix hashcode equals

--- Java/ProjetoPizzaria_fase1/src/entities/Ingrediente.java
+++ Java/ProjetoPizzaria_fase1/src/entities/Ingrediente.java
@@ -29,4 +29,32 @@ public class Ingrediente {
         return ingrediente;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((ingrediente == null) ? 0 : ingrediente.hashCode());
+        result = prime * result + quantidadeDeVezesPedido;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Ingrediente other = (Ingrediente) obj;
+        if (ingrediente == null) {
+            if (other.ingrediente != null)
+                return false;
+        } else if (!ingrediente.equals(other.ingrediente))
+            return false;
+        if (quantidadeDeVezesPedido != other.quantidadeDeVezesPedido)
+            return false;
+        return true;
+    }
+
 }
