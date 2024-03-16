9a257319ffac59429d6ef2aab881a2c04eacca8f
Lucas Larry <116319211+lucaslarry@users.noreply.github.com>
2024-03-15 21:31:12 -0300

    Delete FrontEnd/fase1-english-master directory

--- FrontEnd/fase1-english-master/principal/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('indexForm').addEventListener('submit', function (event) {
-        event.preventDefault();
-        window.location.href = 'greeting.html';
-    });
-});
